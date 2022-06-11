658be4fff19484a2cf98979b7eaf84deb4c3de13
Jiwon <zest1923@gmail.com>
2022-06-12 04:30:30 +0900

    feat: iTunes API 호출 기능

--- MusicAPITest/ContentView.swift
+++ MusicAPITest/ContentView.swift
@@ -7,33 +7,75 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Music: Hashable, Codable {
+    var artistName: String
+    var trackName: String
+    var albumName: String
+    var releaseDate: String
+    var previewUrl: String
+}
+
+class ViewModel: ObservableObject {
+    @Published var musicList: [Music] = []
     
-    @State var searchTitle: String = ""
-    @State private var words = (1...50).map{ String($0) }
+    func getSearchResults() {
+            guard var urlComponents = URLComponents(string: "https://itunes.apple.com/search") else { return }
+            urlComponents.query = "media=music&entity=song&term=bts"
+            guard let url = urlComponents.url else { return }
+                let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                    if let e = error {
+                        NSLog("error: \(e.localizedDescription)")
+                        return
+                    }
+                    
+                    DispatchQueue.main.async() {
+                        do {
+                            let object = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary
+                            
+                            guard let jsonObject = object else {return}
+                            
+                            let searchResults = jsonObject["results"] as! [NSDictionary]
+                            searchResults.forEach { result in
+                                let searchResult = Music(artistName: result["artistName"] as! String, trackName: result["trackName"] as! String, albumName: result["collectionName"] as! String, releaseDate: result["releaseDate"] as! String, previewUrl: result["previewUrl"] as! String)
+                                self.musicList.append(searchResult)
+                            }
+                        } catch let e as NSError {
+                            print("error: \(e.localizedDescription)")
+                        }
+                    }
+                }
+                task.resume()
+        }
+}
+
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
     var body: some View {
         
-//        GeometryReader { geometry in
-//            List {
-//                ForEach(self.words, id: \.self) { word in
-//                                            Text(word)
-//                                                .padding()
-//                                        }
-//            }.listStyle(.sidebar)
-//                }
-        
-        HStack {
-            TextField("검색어를 입력하세요", text: $searchTitle)
-            Button("Button") {
-                searchTitle = ""
-                callAPI()
-                print("CLICKED")
+        NavigationView {
+            List {
+                ForEach(viewModel.musicList, id: \.self) { music in
+                    HStack {
+                        Image("")
+                            .frame(width: 130, height: 70)
+                            .background(.gray)
+                        VStack {
+                            Text(music.trackName)
+                            Text(music.artistName)
+                        }
+                    }
+                    .padding(3)
+                }
             }
-            .background(.blue)
+            .navigationTitle("Search")
+            .onAppear() {
+                viewModel.getSearchResults()
+            }
         }
     }
-    
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ MusicAPITest/ITunesAPI.swift
@@ -0,0 +1,15 @@
+//
+//  ITunesAPI.swift
+//  MusicAPITest
+//
+//  Created by 이지원 on 2022/06/12.
+//
+
+import Foundation
+import UIKit
+//struct Music: Hashable {
+//    var id: Double
+//    var title: String
+//    var artist: String
+//    var image: URL
+//}
