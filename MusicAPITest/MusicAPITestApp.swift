7ef788c8ac8f00c058e6a8eaf6837c8e6d2c8435
Jiwon <zest1923@gmail.com>
2022-06-11 19:22:13 +0900

    init: 프로젝트 설정

---
+++ MusicAPITest/MusicAPITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAPITestApp.swift
+//  MusicAPITest
+//
+//  Created by 이지원 on 2022/06/11.
+//
+
+import SwiftUI
+
+@main
+struct MusicAPITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
